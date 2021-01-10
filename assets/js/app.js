e71b1a6cb5448351a3b6769616aafb30fa82a564
Deepa <deepa.vadakan@gmail.com>
2021-01-10 17:25:21 -0500

    add files

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+// SVG wrapper width and height
+var svgWidth = 900;
+var svgHeight = 500;
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv("assets/data/data.csv").then(function(censusData) {
+  console.log(censusData)
+});
